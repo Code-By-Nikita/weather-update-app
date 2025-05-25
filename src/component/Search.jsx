0ecc1b40abc91eb252539057b09eb2627fab749c
Nikita Choudhary <nikita.choudhary3108@gmail.com>
2025-05-25 18:02:16 +0530

    fix: update language and search button

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -17,9 +17,28 @@ const Search = ({ onSearch }) => {
         onChange={(e) => setCity(e.target.value)}
         className="px-3 py-2 border rounded"
       />
-      <button type="submit" className="px-4 py-2 bg-sky-500 text-white rounded">
+      <button
+        type="submit"
+        className="
+    px-4 py-2
+    bg-sky-500
+    text-white
+    rounded
+    transition
+    duration-300
+    ease-in-out
+    hover:bg-sky-600
+    hover:scale-105
+    active:bg-sky-700
+    active:scale-95
+    focus:outline-none
+    focus:ring-2
+    focus:ring-sky-400
+    focus:ring-opacity-50
+  ">
         Search
       </button>
+
     </form>
   );
 };
